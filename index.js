c215f0decb303e99d3ac77d3879a9b2f4e439609
unknown <yassinioussama3@gmail.com>
2021-04-02 02:31:03 +0100

    make it better

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(cors());
 
 app.use('/users', userRouters);
 
-//const CONNECTION_URL = 'mongodb+srv://mernstack:mernstack123123@cluster0.rckri.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+app.get('/', (req,res) => {
+    res.send("Hello to users app")
+});
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
